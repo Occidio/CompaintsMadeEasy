e7db7d13a4e15e224f54a2e868e2007ed97baef9
Fraham <frahamp@gmail.com>
2017-10-28 17:31:46 +0100

    Login Page

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// used to create fake backend
+import { routing }        from './app.routing';
+
+import { AlertComponent } from './_directives/index';
+import { AuthGuard } from './_guards/index';
+import { AlertService, AuthenticationService, UserService } from './_services/index';
+import { LoginComponent } from './login/index';
+//import { RegisterComponent } from './register/index';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+        AlertComponent,
+        LoginComponent,
+        //RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
