0086436af594186248ce5410a69db45e711e7ac5
Alex Bamford <Occidio001@gmail.com>
2017-10-29 01:18:35 +0100

    Removal of comment

--- Server/main.js
+++ Server/main.js
@@ -23,30 +23,30 @@ app.use(cors());
 
 app.post('/MakeComplaint', function (req, res) {
 
-    //if (!req.session.user) {
-    //  res.send({
-    //    "success": false,
-    //  "message": "User not logged in"
-    //    });
-    //} else {
+    if (!req.session.user) {
+        res.send({
+            "success": false,
+            "message": "User not logged in"
+        });
+    } else {
 
-    SendSms(req.body);
+        SendSms(req.body);
 
-    var ss = new SuzeService();
-    ss.MakeComplaint(req.body, function (response) {
-        if (response.success) {
-            res.send({
-                "success": true,
-                "message": ""
-            });
-        } else {
-            res.send({
-                "success": false,
-                "message": "Issue with the service request: " + response.message
-            });
-        }
-    });
-    //}
+        var ss = new SuzeService();
+        ss.MakeComplaint(req.body, function (response) {
+            if (response.success) {
+                res.send({
+                    "success": true,
+                    "message": ""
+                });
+            } else {
+                res.send({
+                    "success": false,
+                    "message": "Issue with the service request: " + response.message
+                });
+            }
+        });
+    }
 });
 
 var SendSms = function (complaint) {
@@ -76,7 +76,7 @@ var SendGenericSms = function (complaint) {
     var ss = new SuzeService();
     ss.GetCompanyById(complaint.companyId, function (response) {
         if (response.success) {
-            smsService.SendSMS("Your complaint against "+response.response.companyName + " has been created.", complaint.accountId);          
+            smsService.SendSMS("Your complaint against " + response.response.companyName + " has been created.", complaint.accountId);
         } else {
             smsService.SendSMS("Your complaint has been created.", complaint.accountId);
         }
@@ -87,12 +87,12 @@ var MessageParser = function (str, callback) {
     var space = " ";
 
     var words = str.split(space);
-    
+
     var validWords = [];
-    
-    words.forEach(function(element, index){
-    	if(element === ""){
-			validWords.push(words[index+1].replace(/[^a-z]/g,""))     
+
+    words.forEach(function (element, index) {
+        if (element === "") {
+            validWords.push(words[index + 1].replace(/[^a-z]/g, ""))
         }
     });
 
@@ -122,7 +122,7 @@ app.post('/login', function (req, res) {
 app.post('/register', function (req, res) {
     var ss = new SuzeService();
 
-    ss.GetAccountByEmailAndPassword(req.body.email, req.body.password, function(response) {
+    ss.GetAccountByEmailAndPassword(req.body.email, req.body.password, function (response) {
         if (response.success) {
             res.send({
                 "success": false,
@@ -139,8 +139,8 @@ app.post('/register', function (req, res) {
                 req.body.mobilePhone
             );
 
-        	var ss2 = new SuzeService();
-            ss2.AddAccount(account, function(response) {
+            var ss2 = new SuzeService();
+            ss2.AddAccount(account, function (response) {
                 if (response.success) {
                     res.send({
                         "success": true,
@@ -157,24 +157,24 @@ app.post('/register', function (req, res) {
     });
 });
 
-app.get('/test', function(req, res) {
+app.get('/test', function (req, res) {
     var ss = new SuzeService();
 
-    ss.GetAccountByEmail("test@testington.com", function(response) {
+    ss.GetAccountByEmail("test@testington.com", function (response) {
         console.log("Main.js: got response from SS:");
         console.log(response);
         res.send("done");
     });
 });
 
-app.post('/testSMS', function(req, res) {
+app.post('/testSMS', function (req, res) {
     var ss = new SMSService();
 
     ss.SendSMS(req.body.message, req.body.accountId);
     res.send("done");
 });
 
-app.post('/logout', function(req, res) {
+app.post('/logout', function (req, res) {
     req.session.destroy();
     res.send({
         "success": true,
